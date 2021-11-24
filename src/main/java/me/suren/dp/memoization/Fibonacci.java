d587bfbc72abe53b37e3fddfbd9912a547c0272c
isur3n <surendra.raut@gmail.com>
2021-11-24 11:25:16 -0500

    Removed base class.

--- src/main/java/me/suren/dp/memoization/Fibonacci.java
+++ src/main/java/me/suren/dp/memoization/Fibonacci.java
@@ -10,38 +10,46 @@ import java.util.Map;
  *  * Brute force
  *  * Memoization
  */
-public class Fibonacci extends BaseSolution<Long, Long> {
+public class Fibonacci {
+
+  private final boolean isBruteForce;
 
   public Fibonacci() {
-    super(true);
+    this.isBruteForce = true;
   }
 
   public Fibonacci(boolean isBruteForce) {
-    super(isBruteForce);
+    this.isBruteForce = isBruteForce;
   }
 
   public static void main(String[] args) {
     System.out.println("############### Brute Force");
     Fibonacci f1 = new Fibonacci();
-    f1.perform(1L);
-    f1.perform(10L);
-    f1.perform(42L);
+    f1.perform(1);
+    f1.perform(10);
+    f1.perform(42);
 
     System.out.println("############### Optimized");
     Fibonacci f2 = new Fibonacci(false);
-    f2.perform(1L);
-    f2.perform(10L);
-    f2.perform(42L);
+    f2.perform(1);
+    f2.perform(10);
+    f2.perform(42);
   }
 
-  @Override
-  public Long bruteForce(Long n) {
+  public void perform(int n) {
+    Instant start = Instant.now();
+    Long fib = isBruteForce ? bruteForce(n) : memoized(n, new HashMap<>());
+    Instant end = Instant.now();
+    System.out.println("Fibonacci at " + n + " is = " + fib + ", took " + Duration
+        .between(start, end).toMillis() + " msecs.");
+  }
+
+  public Long bruteForce(int n) {
     if(n <= 1) return 1L;
     return bruteForce(n-1) + bruteForce(n-2);
   }
 
-  @Override
-  public Long memoized(Long n, Map<Long, Long> memo) {
+  public Long memoized(int n, Map<Integer, Long> memo) {
     Long value = memo.get(n);
     if(value != null) return value;
 
